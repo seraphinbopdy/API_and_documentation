f228e285e489831d1c63f95eed2e4223f614b1b7
seraphinbopdy <seraphin.noche@yahoo.fr>
2024-08-06 22:42:39 +0200

    Initial commit

---
+++ src/main/java/documentation/fr/api_and_swagger/ApiAndSwaggerApplication.java
@@ -0,0 +1,13 @@
+package documentation.fr.api_and_swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAndSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAndSwaggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/documentation/fr/api_and_swagger/ApiAndSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package documentation.fr.api_and_swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAndSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
